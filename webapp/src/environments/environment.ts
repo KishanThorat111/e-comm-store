6c58a0970667958ff79931e625a26e120dff4813
KishanThorat111 <KishanThorat111@gmail.com>
2025-02-01 13:22:20 +0000

    fix3

--- webapp/src/environments/environment.ts
+++ webapp/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
         // apiUrl:"http://localhost:8080"
-        apiUrl:"https://nodejsbackend.azurewebsites.net/"
+        apiUrl:"https://nodejsbackend.azurewebsites.net"
 };
