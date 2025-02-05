1ac4b76cb4bf4e1c3992730718e6d53ec51a995b
KishanThorat111 <KishanThorat111@gmail.com>
2025-02-05 20:13:54 +0000

    typo error in apiurl link

--- webapp/src/environments/environment.ts
+++ webapp/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
         // apiUrl:"http://localhost:8080"
         // apiUrl:"https://nodejsbackend.azurewebsites.net"
-        apiUrl:"https://nodejsbackend.azurewebsites.net/"
+        apiUrl:"https://nodejsbackend.azurewebsites.net"
 };
