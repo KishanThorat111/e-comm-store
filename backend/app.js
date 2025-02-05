6bdb679f99d8c14f667fe9f252b6888038b6f5dc
KishanThorat111 <KishanThorat111@gmail.com>
2025-02-05 21:54:51 +0000

    updates in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const MongoStore = require("connect-mongo");
 const app = express();
 const port = 8080;
 const cors = require("cors");
@@ -13,6 +14,26 @@ const { verifyToken, isAdmin } = require("./middleware/auth-middleware");
 
 app.set("trust proxy", 1);
 
+
+//mongodb://localhost:27017
+// MongoDB connection
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_CONNECTION, {
+      dbName: "e-comm-store-db",
+    });
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+connectDB();
+
+
+app.use(express.json());
+
 // CORS configuration
 const allowedOrigins = [
   "https://victorious-forest-0704fbe00.4.azurestaticapps.net",
@@ -22,15 +43,14 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-  // origin: (origin, callback) => {
-  //   // Allow requests with no origin (like mobile apps or Postman)
-  //   if (!origin || allowedOrigins.includes(origin)) {
-  //     callback(null, true); // Allow the origin
-  //   } else {
-  //     callback(new Error("Not allowed by CORS")); // Deny the origin
-  //   }
-  // },
-  origin: "*",
+  origin: (origin, callback) => {
+    // Allow requests with no origin (like mobile apps or Postman)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true); // Allow the origin
+    } else {
+      callback(new Error("Not allowed by CORS")); // Deny the origin
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow all HTTP methods
   credentials: true, // Allow credentials (cookies, headers, etc.)
   preflightContinue: false, // Continue handling preflight requests
@@ -40,7 +60,7 @@ const corsOptions = {
 // Enable CORS for the application
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
-app.use(express.json());
+// app.use(express.json());
 app.get("/", (req, res) => {
   res.send("server running");
 });
@@ -52,18 +72,18 @@ app.use("/product", verifyToken, isAdmin, productRoutes);
 app.use("/customer", verifyToken, customerRoutes);
 app.use("/auth", authRoutes);
 
-//mongodb://localhost:27017
-async function connectDB() {
-  await mongoose.connect(process.env.MONGO_CONNECTION, {
-    dbName: "e-comm-store-db",
-  });
-  console.log("MongoDB connected");
-}
+// //mongodb://localhost:27017
+// async function connectDB() {
+//   await mongoose.connect(process.env.MONGO_CONNECTION, {
+//     dbName: "e-comm-store-db",
+//   });
+//   console.log("MongoDB connected");
+// }
 
-connectDB().catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
+// connectDB().catch((err) => {
+//   console.error(err);
+//   process.exit(1);
+// });
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
