828181e08c259e33e6341b8e677a18749bfee2f5
KishanThorat111 <KishanThorat111@gmail.com>
2025-03-06 06:57:03 +0000

    added vercel URL

--- webapp/src/environments/environment.ts
+++ webapp/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
         // apiUrl:"http://localhost:8080"
         // apiUrl:"https://nodejs-hgtc.onrender.com"
-        apiUrl:"https://nodejsbackend.azurewebsites.net"
+        // apiUrl:"https://nodejsbackend.azurewebsites.net"
+        apiUrl:"https://e-comm-store-backend-back4app-o3v6vr056.vercel.app/"
 };
